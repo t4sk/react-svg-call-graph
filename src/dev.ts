9eb2311e69ab5b66a7a7a30df8181a83b95827d0
t4sk <tsk.nakamura@gmail.com>
2025-05-20 10:04:45 +0900

    tmp

--- src/dev.ts
+++ src/dev.ts
@@ -6,6 +6,7 @@ type Trace = {
 }
 
 type Graph = Map<number, Set<number>>
+type Parents = Map<number, number>
 
 const trace: Trace[] = [
   { id: 0, parent: null, depth: 0, children: [1, 2, 3, 4] },
@@ -24,13 +25,15 @@ const trace: Trace[] = [
   { id: 4, parent: 0, depth: 1, children: [14, 15, 16] },
   { id: 14, parent: 4, depth: 2, children: null },
   { id: 15, parent: 4, depth: 2, children: null },
-  { id: 16, parent: 4, depth: 2, children: [0] },
-  { id: 0, parent: 16, depth: 3, children: [99] },
-  { id: 99, parent: 0, depth: 4, children: null },
+  { id: 16, parent: 4, depth: 2, children: null },
+  // { id: 16, parent: 4, depth: 2, children: [0] },
+  // { id: 0, parent: 16, depth: 3, children: [99] },
+  // { id: 99, parent: 0, depth: 4, children: null },
 ]
 
-function build(trace: Trace[]): Graph {
+function build(trace: Trace[]): {graph: Graph, parents: Parents} {
   const graph: Graph = new Map()
+  const parents: Parents = new Map()
 
   for (let i = 0; i < trace.length; i++) {
     const t = trace[i]
@@ -42,11 +45,12 @@ function build(trace: Trace[]): Graph {
     if (t.children) {
       for (const v of t.children) {
         graph.get(t.id)?.add(v)
+        parents.set(v, t.id)
       }
     }
   }
 
-  return graph
+  return { graph, parents }
 }
 
 function bfs(graph: Graph, start: number, f?: (i: number, v: number) => void) {
@@ -78,30 +82,90 @@ function bfs(graph: Graph, start: number, f?: (i: number, v: number) => void) {
   }
 }
 
-const graph = build(trace)
-console.log(graph)
+function dfs(
+  graph: Graph,
+  start: number,
+  f?: (d: number, v: number) => void,
+): boolean {
+  type Node = {p: number | null; v: number }
+
+  const q: Node[] = [{ p: null, v: start }]
+  const visited: Set<number> = new Set()
+  const path: number[] = []
+
+  while (q.length > 0) {
+    // Avoid using shift() which is O(N) to get element from the head of q
+    const { p, v } = q.pop() as Node
+
+    // Cycle detected - invalid DAG
+    if (visited.has(v)) {
+      return false
+    }
+
+    // Backtrack path to p
+    while (path.length > 0 && path[path.length - 1] != p) {
+      const u = path.pop() as number
+      visited.delete(u)
+    }
+
+    visited.add(v)
+    path.push(v)
+
+    if (f) {
+      f(path.length - 1, v)
+    }
+
+
+    const neighbors = graph.get(v)
+    if (neighbors) {
+      for (const w of [...neighbors].reverse()) {
+        q.push({ p: v, v: w })
+      }
+    }
+  }
+
+  // No cycles - valid DAG
+  return true
+}
+
+const { graph, parents } = build(trace)
+console.log(graph, parents)
 
 /*
-bfs(graph, 0, (i, v) => {
-  console.log(i, v, graph.get(v))
+const pos: Map<number, number> = new Map()
+
+bfs(graph, 0, (d, v) => {
+  console.log(d, v, graph.get(v))
+
   const neighbors = graph.get(v)
   if (neighbors) {
     const n = neighbors.size
-    console.log(n, n >> 1)
+    const mid = n >> 1
+
+    // Positions relative to parent
+    let i = 0
+    if (n % 2 == 0) {
+      for (const w of neighbors) {
+        pos.set(w, mid - i - Math.floor(i / mid))
+        i++
+      }
+    } else {
+      for (const w of neighbors) {
+        pos.set(w, mid - i)
+        i++
+      }
+    }
   }
 })
+
+console.log(pos)
 */
 
-const n = 1
-const m = n >> 1
-// Even
 /*
-for (let i = 0; i < n; i++) {
-  console.log(i, m - i - Math.floor(i / m))
-}
+const arr: [number, number][] = []
+dfs(graph, 0, (d, v) => {
+  arr.push([d, v])
+})
+console.log(arr)
 */
 
-// Odd
-for (let i = 0; i < n; i++) {
-  console.log(i, m - i)
-}
